90b6e7857d17ffc8491fe58c7741d15af5fb6e1d
albin-woto <albin@wotoszyn.com>
2021-01-04 18:52:29 -0300

    Add simple validation function for ip through regex

---
+++ src/utils/validation.tsx
@@ -0,0 +1,15 @@
+const validation = (ip: string): boolean => {
+  const regex = new RegExp(
+    '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+  );
+  let validIp = false;
+
+  // Empty ip value in request returns public IP info, that's why is "valid"
+  if (regex.test(ip) || ip === '') {
+    validIp = true;
+  }
+
+  return validIp;
+};
+
+export default validation;
