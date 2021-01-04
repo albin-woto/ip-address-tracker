5791550f90e3cf8819dbeb7f3bfe5ed99e5f0fc6
albin-woto <albin@wotoszyn.com>
2021-01-04 18:57:44 -0300

    Integrate the context provider for IpInfo in App

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { IpContextProvider } from '../context/IpContext';
+
 import Layout from '../components/Layout';
 import Home from '../pages/Home';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Layout>
-    </BrowserRouter>
+    <IpContextProvider>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Layout>
+      </BrowserRouter>
+    </IpContextProvider>
   );
 };
 
