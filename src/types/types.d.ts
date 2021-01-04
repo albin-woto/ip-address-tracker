9d02acbb2645e25505862903fce3f5ad0e6ed8ad
albin-woto <albin@wotoszyn.com>
2021-01-04 18:55:09 -0300

    Add types for object IpInfo and IpContext

---
+++ src/types/types.d.ts
@@ -0,0 +1,23 @@
+export default interface IpInfo {
+  // code only in error
+  code?: number;
+  ip?: string;
+  location?: {
+    country: string;
+    region: string;
+    city: string;
+    lat: number;
+    lng: number;
+    postalCode: string;
+    timezone: string;
+    geonameId: number;
+  };
+  as?: object;
+  isp?: string;
+  proxy?: object;
+}
+
+export type IpContext = {
+  ipInfo: IpInfo;
+  setIpInfo: (ipInfo: IpInfo) => void;
+};
