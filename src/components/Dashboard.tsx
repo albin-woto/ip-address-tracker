e5b756fdc32b2cbda8ca0f6ee823fb53b10deefb
albin-woto <albin@wotoszyn.com>
2021-01-04 21:43:09 -0300

    Connect context to Dashboard, handle error and display corresponding info

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../assets/styles/components/Dashboard.scss';
+import IpContext from '../context/IpContext';
 
 const Dashboard: React.FC = () => {
+  const { ipInfo } = useContext(IpContext);
+  const { ip, location, isp, code } = ipInfo;
+
   return (
     <section className="dashboard">
+      {
+        // The free API has a limit, here I handle that specific error
+        code === 403 && (
+          <h2 className="dashboard__error">
+            I'm sorry, request limit reached.
+          </h2>
+        )
+      }
       <ul className="dashboard__list">
         <li className="dashboard__item">
           <h1 className="item__title">IP ADDRESS</h1>
-          <p className="item__value">112.123.123</p>
+          <p className="item__value">{ip || 'Not available'}</p>
         </li>
         <li className="dashboard__item">
           <h1 className="item__title">LOCATION</h1>
-          <p className="item__value">La Quiaca</p>
+          <p className="item__value">{location?.city || 'Not available'}</p>
         </li>
         <li className="dashboard__item">
           <h1 className="item__title">TIMEZONE</h1>
-          <p className="item__value">1:11</p>
+          <p className="item__value">{location?.timezone || 'Not available'}</p>
         </li>
         <li className="dashboard__item">
           <h1 className="item__title">ISP</h1>
-          <p className="item__value">Lenteja</p>
+          <p className="item__value">{isp || 'Not available'}</p>
         </li>
       </ul>
     </section>
