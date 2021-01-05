e2d9c7d6dccd3e7d0ca4a0b2c8e19e7227da24f0
albin-woto <albin@wotoszyn.com>
2021-01-05 13:11:51 -0300

    New Loader with styles

---
+++ src/components/Loader.tsx
@@ -0,0 +1,14 @@
+import '../assets/styles/components/Loader.scss';
+
+const Loader = () => (
+  <div className="loader__container">
+    <div className="loader__ellipsis">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+);
+
+export default Loader;
