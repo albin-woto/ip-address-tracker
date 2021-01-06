4eeda2ec85402ef0af3fdd6472b2e166ed5587c3
albin-woto <albin@wotoszyn.com>
2021-01-05 21:10:06 -0300

    Create Map component with styles
    - New map component using leaflet.
    - Use effect to update location from context.
    - Create a default location to use until/in case I don't get a location from request.

---
+++ src/components/Map.tsx
@@ -0,0 +1,59 @@
+import { useContext, useEffect, useState } from 'react';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  ZoomControl,
+  useMap,
+} from 'react-leaflet';
+import { LatLngExpression } from 'leaflet';
+import { ChangeViewProps } from '../types/types';
+import { VenueLocationIcon } from './VenueLocationIcon';
+import 'leaflet/dist/leaflet.css';
+import '../assets/styles/components/Map.scss';
+import IpContext from '../context/IpContext';
+
+const Map = () => {
+  const defaultLocation = { lat: -34.65114705956248, lng: -58.44001654662209 };
+  const [mapLocation, setMapLocation] = useState<LatLngExpression>(
+    defaultLocation
+  );
+  const { ipInfo } = useContext(IpContext);
+  const { location } = ipInfo;
+
+  // MapContainer props are inmutable, so to change view I have to use a custom function
+  const ChangeView = ({ center, zoom }: ChangeViewProps) => {
+    const map = useMap();
+    map.setView(center, zoom);
+    return null;
+  };
+
+  useEffect(() => {
+    // Check if location was updated by request
+    Object.keys(ipInfo).includes('location') &&
+      setMapLocation({ lat: location?.lat!, lng: location?.lng! });
+  }, [ipInfo, location]);
+
+  return (
+    <MapContainer
+      center={mapLocation}
+      zoom={14}
+      zoomAnimation={true}
+      scrollWheelZoom={false}
+      zoomControl={false}
+    >
+      <ChangeView center={mapLocation} zoom={14} />
+      <ZoomControl position="bottomleft" />
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={mapLocation!} icon={VenueLocationIcon}>
+        <Popup>{ipInfo?.location?.city || 'Default Location'}</Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
