1df37ca170ea453becfc57d709820a5caa3e71ad
albin-woto <albin@wotoszyn.com>
2021-01-11 23:09:48 -0300

    Move request logic into a single custom hook useIp

---
+++ src/hooks/useIp.tsx
@@ -0,0 +1,46 @@
+import { useContext, useState, useEffect } from 'react';
+import IpContext from '../context/IpContext';
+import getIpInfo from '../utils/getIpInfo';
+import validation from '../utils/validation';
+
+export const useIp = (): {
+  ip: string;
+  setIp: React.Dispatch<React.SetStateAction<string>>;
+  isValid: boolean;
+  updateIpInfo: (ip: string) => void;
+} => {
+  const { setIpInfo, setLoading, ipInfo } = useContext(IpContext);
+  const [ip, setIp] = useState<string>('');
+  const [isValid, setIsValid] = useState<boolean>(true);
+
+  // Function that updates context to use in every component
+  const updateIpInfo = (ip: string) => {
+    const storedIp = ipInfo.ip;
+    const validationResult = validation(ip);
+    setIsValid(validationResult);
+
+    // Checking for invalid input and requesting data already stored(i.e. multiple clicks by user)
+    if (validationResult && storedIp !== ip) {
+      setLoading(true);
+
+      getIpInfo(ip).then((ipInfo) => {
+        setIpInfo(ipInfo!);
+        setLoading(false);
+      });
+    }
+  };
+
+  // Get Public IP info ONLY ONCE in initial render because the free API is really limited
+  useEffect(() => {
+    const controller = new AbortController();
+
+    if (Object.entries(ipInfo).length === 0) {
+      updateIpInfo(ip);
+    }
+
+    return () => controller.abort();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { ip, setIp, isValid, updateIpInfo };
+};
