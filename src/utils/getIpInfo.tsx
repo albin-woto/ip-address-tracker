efe585481e21f08f2e8b3dc80b1ae7f53ecc7df6
albin-woto <albin@wotoszyn.com>
2021-01-12 11:55:46 -0300

    Remove optional ip from getIpInfo.

--- src/utils/getIpInfo.tsx
+++ src/utils/getIpInfo.tsx
@@ -2,10 +2,10 @@ import IpInfo from '../types/types';
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
-const getIpInfo = async (ip?: string) => {
+const getIpInfo = async (ip: string) => {
   try {
     const response = await fetch(
-      // If I make a request without ip, it will return the public IP info
+      // If I make a request without ip(''), it will return the public IP info
       ip
         ? `https://geo.ipify.org/api/v1?apiKey=${API_KEY}&ipAddress=${ip}`
         : `https://geo.ipify.org/api/v1?apiKey=${API_KEY}`
