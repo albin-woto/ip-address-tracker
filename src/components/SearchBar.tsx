d46c0936909aad1f593bd165f6a232a309405f36
albin-woto <albin@wotoszyn.com>
2021-01-11 23:11:09 -0300

    Clean component SearchBar and integrate new custom hook useIp

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,29 +1,9 @@
-import React, { useState, useEffect, useContext } from 'react';
-import IpContext from '../context/IpContext';
-import validation from '../utils/validation';
-import getIpInfo from '../utils/getIpInfo';
 import arrowIcon from '../assets/images/icon-arrow.svg';
 import '../assets/styles/components/SearchBar.scss';
+import { useIp } from '../hooks/useIp';
 
 const SearchBar: React.FC = () => {
-  const [isValid, setIsValid] = useState<boolean | undefined>(undefined);
-  const [ip, setIp] = useState<string>('');
-  const { setIpInfo, setLoading, ipInfo } = useContext(IpContext);
-
-  // Get Public IP info ONLY in initial render because the free API is really limited
-  useEffect(() => {
-    const controller = new AbortController();
-
-    if (Object.entries(ipInfo).length === 0) {
-      getIpInfo(ip).then((publicIpInfo) => {
-        setIpInfo(publicIpInfo!);
-        setLoading(false);
-      });
-    }
-
-    return () => controller.abort();
-    // eslint-disable-next-line
-  }, []);
+  const { ip, setIp, isValid, updateIpInfo } = useIp();
 
   // Handle user input
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,18 +12,7 @@ const SearchBar: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Validate ip input
-    const validationResult = validation(ip);
-    setIsValid(validationResult);
-
-    if (isValid && ipInfo.ip !== ip) {
-      setLoading(true);
-
-      getIpInfo(ip).then((ipInfo) => {
-        setIpInfo(ipInfo!);
-        setLoading(false);
-      });
-    }
+    updateIpInfo(ip);
   };
 
   return (
@@ -55,9 +24,7 @@ const SearchBar: React.FC = () => {
           value={ip}
           onChange={handleChange}
           placeholder="Search for any IP address or domain"
-          className={`search__input ${
-            !isValid && isValid !== undefined ? 'search__input--error' : ''
-          }`}
+          className={`search__input ${!isValid ? 'search__input--error' : ''}`}
           aria-label="Input to search for any IP address or domain"
           required
         />
@@ -69,7 +36,7 @@ const SearchBar: React.FC = () => {
           <img src={arrowIcon} alt="arrow icon" />
         </button>
       </form>
-      {!isValid && isValid !== undefined && (
+      {!isValid && (
         <h2 className="search__error">
           <span role="img" aria-label="red cross">
             ❌
