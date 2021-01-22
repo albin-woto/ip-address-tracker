e5c6b5ce9938ef848a73a914ce4b214b1fad1aa1
albin-woto <albin@wotoszyn.com>
2021-01-22 13:25:24 -0300

    Change regex declaration in validateIp

--- src/utils/validateIp.tsx
+++ src/utils/validateIp.tsx
@@ -1,8 +1,7 @@
 const validateIp = (ip: string): boolean => {
-  const regex = new RegExp(
-    '^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
-  );
-  let validIp = false;
+  const regex: RegExp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?).){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+
+  let validIp: boolean = false;
 
   // Empty ip value in request returns public IP info, that's why it is "valid"
   if (regex.test(ip) || ip === '') {
